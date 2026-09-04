73f0ac44185c6fd85f6d51f5261944248e2a0d74
LzKitKoding <lzkitkoding@gmail.com>
2026-09-04 13:24:16 +1200

    Completed vanilla JavaScript project setup.

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+const app = document.querySelector('#app');
+
+app.innerHTML = `
+    <div class="container">
+        <h1>StoryForge</h1>
+        <p>Vanilla JavaScript setup successful.</p>
+    </div>
+`;
